f74d82fc002cbbc6e8fb40ae95d1db32fba71419
Antonio Pardo <apardo@alabs.es>
2011-06-28 23:32:42 +0200

    ficheros de jquery-rails

---
+++ public/javascripts/application.js
@@ -0,0 +1,8 @@
+$(function() {
+  
+  // Ocultar el span de mensajes flash pasado 10 segundos
+  $("#flash-notice span, #flash-alert span").fadeOut(30000);
+  
+  // Poner el foco en el formulario en la página de login y registro
+  $("#login-form #user_email, #signup-form #user_email").focus();
+});
