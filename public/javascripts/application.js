9efe691006cc205ad44e96b03e6d92000b232f46
anders <anders@parker.(none)>
2011-06-29 17:05:48 +0200

    Conteo de mensajes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,4 +5,19 @@ $(function() {
   
   // Poner el foco en el formulario en la página de login y registro
   $("#login-form #user_email, #signup-form #user_email, #new-message-form #message_body").focus();
+
+
+  // Cuenta regresiva de caracteres de mensajes, el limite es 140
+  $('textarea#message_body').keyup(function() {
+    var charLength = $(this).val().length;
+    // Displays count
+    $('#caracter-counter').html(charLength);
+    console.log(charLength);
+    
+    // Alerts when 140 characters is reached
+    if($(this).val().length > 140) {
+      $('#caracter-counter').html('<strong>Hola apardo.</strong>');
+    } 
+  });
+
 });
