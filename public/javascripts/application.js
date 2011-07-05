02c3c4872bdcf9ca41252c0f9e660554130f9e32
Andres Pereira <apereira@alabs.es>
2011-07-05 14:16:08 +0200

    Widget de Uservoice

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -46,3 +46,12 @@ $(function() {
   // slideshow en la home (final)
 
 });
+
+// Widget para Uservoice
+(function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/qiMECk3hVp3UZ7RV7DH5g.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+})();
+
+
