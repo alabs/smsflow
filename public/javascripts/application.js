47c0eabedbf1cc16db1069bf256ece17376403f1
Antonio Pardo <apardo@alabs.es>
2011-07-05 05:48:13 +0200

    marcado para la página de mensajes hecho, falta el css

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,22 +4,22 @@ $(function() {
   $("#flash-notice span, #flash-alert span").fadeOut(30000);
   
   // Poner el foco en el formulario en la página de login y registro
-  $("#login-form #user_email, #signup-form #user_email, #new-message-form #message_destination").focus();
+  $("#login-form #user_email, #signup-form #user_email, #message-form #message_body").focus();
 
 
   // Cuenta regresiva de caracteres de mensajes, el limite es 140
   $('textarea#message_body').keyup(function() {
     var charLength = $(this).val().length;
     // Displays count
-    $('#caracter-counter').html(charLength);
+    $('#counter').html(charLength);
     
     // Alerts when 140 characters is reached
     if($(this).val().length > 140) {
-      $('#caracter-counter').addClass('wrong-count');
-      $('#caracter-counter').html(140 - charLength);
+      $('#counter').addClass('wrong-count');
+      $('#counter').html(140 - charLength);
       $('#message_submit').attr('disabled', true).css('background-color','grey');
     } else {
-      $('#caracter-counter').removeClass('wrong-count');
+      $('#counter').removeClass('wrong-count');
       $('#message_submit').attr('disabled', false).css('background-color','#416CC4');
     }
   });
